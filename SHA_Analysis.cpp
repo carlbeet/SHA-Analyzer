f90bb351fe1e1d0071b96cdf5abdcf27f4811499
carlbeet <carleys.mailbox@gmail.com>
2022-04-20 23:00:22 -0400

    update

--- SHA_Analysis.cpp
+++ SHA_Analysis.cpp
@@ -13,42 +13,23 @@
 #include <stdio.h>  //* printf *
 using namespace std;
 
+/*resources! :)
 
-
-//resources! :)
-/*
 Testing:
 http://csrc.nist.gov/groups/ST/toolkit/documents/Examples/SHA256.pdf
 NIST:
 https://csrc.nist.gov/csrc/media/publications/fips/180/2/archive/2002-08-01/documents/fips180-2.pdf
 Terminology:
 groups of 32 bits are called "words"
+
 NIST: "1. A hex digit is an element of the set {0, 1,…, 9, a,…, f}. A hex digit is the
 representation of a 4-bit string. For example, the hex digit “7” represents the 4-bit
 string “0111”, and the hex digit “a” represents the 4-bit string “1010”.
+
 2.	 A word is a w-bit string that may be represented as a sequence of hex digits. To
 convert a word to hex digits, each 4-bit string is converted to its hex digit equivalent,
 as described in (1) above. For example, the 32-bit string "
-string createHash(vector<string>& block) {
-return "";
-}
-// hardcoded hash things
-// 	k = {0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,
-// 		0x923f82a4,0xab1c5ed5,0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,
-// 		0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,0xe49b69c1,0xefbe4786,
-// 		0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
-// 		0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,
-// 		0x06ca6351,0x14292967,0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,
-// 		0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,0xa2bfe8a1,0xa81a664b,
-// 		0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
-// 		0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,
-// 		0x5b9cca4f,0x682e6ff3,0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,
-// 		0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2 }
 */
-//These words represent the first 32
-//bits of the fractional parts of the cube roots of the first sixty-
-//four prime numbers.
-
 //=======================================================================================================
 //source: https://csrc.nist.gov/csrc/media/publications/fips/180/2/archive/2002-08-01/documents/fips180-2.pdf
 
@@ -157,8 +138,6 @@ map<string, string> SHA2(map<string, string>& passwords, vector<unsigned long>& 
     }
     return encrypted;
 }
-
-
 
 //================================= SHA 256 Helper functions ======================================
 
@@ -339,7 +318,7 @@ if (isprime(n)) {
 
 }
 
-string decToHex(long double number) //double to hex
+string decToHex(long double number) //double to hex string
 {   
     number = number * pow(2, 32);
     long long  n = (long long) (floor(number));
@@ -378,6 +357,8 @@ string decToHex(long double number) //double to hex
 }
 
 string decToHex(int number) //overloaded method, this one is int to hex
+// input: int number
+// output: string of hex value
 {   
     int n = number; //any typecasting should go here.
     int temp;
@@ -412,6 +393,8 @@ string decToHex(int number) //overloaded method, this one is int to hex
 
 string decToBinary(int number)
 {
+    // input: int number
+    // output: string of binary digits
     string temp = "";
     string binary = "";
     
